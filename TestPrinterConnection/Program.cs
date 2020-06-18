f919611d1c79c6788e87c6237d8074d454889337
prateeksriv1998 <prateeksriv1998@gmail.com>
2020-06-18 14:22:56 +0530

    initial commit

---
+++ TestPrinterConnection/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestPrinterConnection
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please Enter the IP Address:");
+                string host = Console.ReadLine();
+                Console.WriteLine("Please Enter the Port Number:");
+                int port = Convert.ToInt32(Console.ReadLine());
+                IPAddress[] IPs = Dns.GetHostAddresses(host);
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine($"Establishing Connection to {host}");
+                s.Connect(IPs, port);
+                Console.WriteLine("Connection Established.");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
